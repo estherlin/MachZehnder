c53f011bb74d03e344ff0209fd596a077bf9bf98
esther lin <esther.lin@alumni.ubc.ca>
2018-06-13 21:21:00 +0200

    Using a template of test.js

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    if (!Detector.webgl) {
+
+        Detector.addGetWebGLMessage();
+        document.getElementById('container').innerHTML = "";
+
+    }
+
+    // global variables
+
+    /*
+     * Creates the scene by assigning scene, camera and renderer
+     *
+     */
+    function init() {
+
+
+    }
+
+    /*
+     * Renders the scene 
+     *
+     */
+    function animate() {
+
+
+    }
+
+
+
+
+});
